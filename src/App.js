2d8c640f58f3108f8a81d6a9345ef360599ae884
Sierra Zang <sierrazang523@gmail.com>
2021-08-19 23:47:04 -0400

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-// App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-//language = js
-
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <Router>
+      <Switch>
+        <Route path="/" exact render={() => <h1>Hello world</h1>} />
+        <Route render={() => <h1>404 Not found</h1>} />
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
