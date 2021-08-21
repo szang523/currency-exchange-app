59cbc62a02a178665e2cf4fc4f330888eabc56a8
Sierra Zang <sierrazang523@gmail.com>
2021-08-20 23:44:38 -0400

    tweaks to layout styling

--- src/layout.js
+++ src/layout.js
@@ -3,14 +3,14 @@ const Layout = (props) => {
   return (
     <React.Fragment>
       <nav className="navbar justify-content-center">
-        <span className="h1">Currency Exchange Rates</span>
+        <span className="h1 py-2 m-2 text-center">Currency Converter & Exchange Rates</span>
       </nav>
       <div className="container py-3">
         {props.children}
       </div>
       <footer className="footer text-center p-3">
         <div>
-          <span>Portfolio: <a href="" target="_blank">xyz@example.com</a></span>
+          <span className="text-white">Portfolio: <a className="text-white" href="" target="_blank">xyz@example.com</a></span>
         </div>
       </footer>
     </React.Fragment>
