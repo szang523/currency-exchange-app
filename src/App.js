e4e02ea30f5a10e6ff514854a8818b5983cc4094
Sierra Zang <sierrazang523@gmail.com>
2021-08-21 23:47:52 -0400

    updated home section (base and add currency options

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './layout';
+import Home from './home';
 
 import './App.css';
 
@@ -9,7 +10,7 @@ const App = () => {
     <Router>
       <Layout>
         <Switch>
-          <Route path="/" exact render={() => <h1>Hello world</h1>} />
+          <Route path="/" exact component={Home} />
           <Route render={() => <h1>404 Not found</h1>} />
         </Switch>
       </Layout>
